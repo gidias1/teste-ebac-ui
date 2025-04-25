3103eb1d26b602e281feb7b4d7c00c49a163e0ed
gidias1 <giovanna2001.dias@gmail.com>
2025-04-25 12:21:01 -0300

    mudança no gitignore

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "gz112k",
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     baseUrl: 'http://lojaebac.ebaconline.art.br',
+    video:true,
   },
 });
--- cypress/e2e/loja-ebac/produtos.cy.js
+++ cypress/e2e/loja-ebac/produtos.cy.js
@@ -1,18 +1,48 @@
 /// <reference types="cypress"/>
 
+import produtosPage from "../../support/page-objects/produtos.page";
+
 describe('Funcionalidade: Produtos', () => {
 
 beforeEach(() => {
-    cy.visit('produtos')
+    produtosPage.visitarUrl()
 });
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.product-block ')
-        //.first()
-        //.last()
-        //.eq(2)
-        .contains('Ajax Full-Zip Sweatshirt')
-        .click()
+        produtosPage.buscarProdutoLista('Aether Gym Pant')
 
         cy.get('#tab-title-description > a').should('contain' , 'Descrição')
     });
+
+    it('Deve buscar um produto com sucesso', () => {
+        let produto = 'Augusta Pullover Jacket'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain' , produto)
+    });
+
+    it('Deve visitar a página do produto', () => {
+        produtosPage.visitarProduto('Ariel-Roll-Sleeve-Sweatshirt')
+        
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
+        produtosPage.buscarProduto('Ariel Roll Sleeve Sweatshirt')
+        produtosPage.addProdutoCarrinho('L', 'Red', '4')
+
+        cy.get('.woocommerce-message').should('contain' , 'foram adicionados no seu carrinho.')
+    });
+
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+        produtosPage.buscarProduto(dados[0].nomeProduto)
+        produtosPage.addProdutoCarrinho(
+            dados[0].tamanho, 
+            dados[0].cor, 
+            dados[0].quantidade)
+        cy.get('.product_title').should('contain' , dados[1].nomeProduto)
+
+        })
+        
+    });
+
+    
 });
